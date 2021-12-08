5957e825c2d3bdc0b92f12879552cd2debda53dd
chonmein <chonmein@gmail.com>
2021-12-08 17:58:21 +0800

    三角形實作

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public interface ICarBehavior
+    {
+        void BaBu();
+    }
+    public class Car : ICarBehavior
+    {
+        private string name;
+
+        public Car(string name, int tire = 0, int door = 0)
+        {
+            Name = name;
+            Tire = tire;
+            Door = door;
+        }
+        //public Car(int tire,int door)
+        //{
+        //    Tire = tire;
+        //    Door = door;
+        //}
+        //public Car(int tire, string name, int door)
+        //{
+        //    Name = name;
+        //    Tire = tire;
+        //    Door = door;
+        //}
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Tire { get; set; }
+        public int Door { get; set; }
+
+        public void BaBu()
+        {
+            Console.WriteLine("喇叭聲音");
+        }
+
+        public void Run()
+        {
+
+        }
+        public void Run(string message)
+        {
+
+        }
+    }
+
+    public class Airplane : Car
+    {
+        public Airplane(string name, int tire = 0, int door = 0) : base(name, tire, door)
+        {
+
+        }
+        public void Fly()
+        {
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,107 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("這裡可以輸出");
-            Console.ReadLine();
+            //string Message01 = "請輸入整數A";
+            //string Message02 = "請輸入整數B";
+            //int numberA = 0;
+            //int numberB = 0;
+            //int answer = 0;
+            //Console.WriteLine(Message01);
+            //numberA = ReadInt();
+            //Console.WriteLine(Message02);
+            //numberB = ReadInt();
+            //answer = numberA + numberB;
+            //Console.WriteLine(answer);
+            //Console.ReadLine();
+
+            ////for(var i = 0; i < answer; i++)
+            ////{
+            ////    if (i == 5)
+            ////        continue; 
+            ////        // break;
+            ////    Console.WriteLine(i);
+            ////}
+
+            ////var numbers =  Enumerable.Range(5, 10).ToArray();
+            ////foreach (var number in numbers)
+            ////{
+            ////    Console.WriteLine(number);
+            ////}
+
+
+            ////while (true)
+            ////{
+            ////    var message = Console.ReadLine();
+            ////    Console.WriteLine($"您輸入{message}");
+            ////}
+
+            //do
+            //{
+            //    //to do
+            //} while (false);
+
+            //switch (answer)
+            //{
+            //    case 0:
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //var msg = answer > 10 ? "大於10喔" : "小於等於10喔";
+            //Console.WriteLine(msg);
+
+            //var bike = new Car("腳踏車", 2, 0);
+            //var bike02 = new Car("腳踏車");
+            ////var bike01 = new Car(2, 0);
+            ////var bike02 = new Car(2, "腳踏車", 0);
+
+            //bike.Run("");
+            //bike.BaBu();
+
+            //var airplane = new Airplane("腳踏車");
+            //airplane.Run();
+            //airplane.Fly();
+            //airplane.BaBu();
+            //Console.ReadLine();
+
+            //如果使用者輸入多少，那麼就出現底為多少的三角形
+            while (true)
+            {
+                Console.WriteLine("請輸入三角形的底邊長");
+                int input = Convert.ToInt32(Console.ReadLine());
+                var numberList = Enumerable.Range(1, input);
+                foreach (int number in numberList)
+                {
+                    for (int i = 0; i < input - number; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int i = 0; i < number; i++)
+                    {                        
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static int ReadInt()
+        {
+            int number = 0;
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("format");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("overflow");
+            }
+            return number;
         }
     }
 }
